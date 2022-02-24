a9a952bfb1c26c2b3cbe8d26a3dd3c99fa4d6a76
MoisesDuarte <moisesduartejunior@gmail.com>
2022-02-24 19:41:55 -0300

    Adjusting gitignore files and eslintrules + creating client side dockerfile

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -8,5 +8,6 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   rules: {
     "vue/require-default-prop": "off",
+    "no-useless-catch": "off",
   },
 };
--- client/src/resources/ApiResource.js
+++ client/src/resources/ApiResource.js
@@ -3,12 +3,16 @@ import axios from 'axios';
 class ApiResource {
   baseUrl = import.meta.env.VITE_SERVER_HOST;
 
+   /**
+   * Get pokemon by name
+   * @param {string} name - A string to filter name by
+   * @returns an object with the pokemon information
+   */
   async getPokemonByName(name) {
     try {
       const response = await axios.get(`${this.baseUrl}/pokemon/${name}`);
       return response.data;
     } catch (err) {
-      console.error(err);
       throw err;
     }
   }
