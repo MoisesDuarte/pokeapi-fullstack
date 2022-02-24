bc5eb25ebb5e6960042cf4362808bd498bd2405e
MoisesDuarte <moisesduartejunior@gmail.com>
2022-02-24 17:37:32 -0300

    Setting up eslint + prettier in frontend project

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
